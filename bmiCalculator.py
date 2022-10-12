a0e2397875fd798693a8f509385690636ddaf49b
Samuel Lundandi <102692513+mrlundandi@users.noreply.github.com>
2022-10-12 11:35:31 +0200

    Delete bmiCalculator.py

--- bmiCalculator.py
+++
@@ -1,8 +0,0 @@
-weigth = input("What's your weight in kg? ")
-height = input("What's your height in meters? ")
-bmi = round(float(weigth)/float(height)**2, 2)
-print("Your body mass index (BMI) is " + str(bmi))
-
-'''Escribe un programa en la consola de python que pida al usuario su peso (en kg) y estatura (en metros), 
-calcule el índice de masa corporal y lo almacene en una variable, e imprima por pantalla la frase 
-"Tu índice de masa corporal es ", donde es el índice de masa corporal calculado redondeado con dos decimales.'''
